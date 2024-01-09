217bc20d20a82d2e91231f8205046ef56bd892e1
over <over@MacBook-Pro-de-over.local>
2024-01-09 11:42:15 -0300

    update

--- consulta-bd-cloud.py
+++ consulta-bd-cloud.py
@@ -23,7 +23,7 @@ HTML_TEMPLATE = """
 <body>
     <h2>Executar Consulta SQL</h2>
     <form method="POST">
-        <textarea name="sql_query" rows="20" cols="150">{{ sql_query if sql_query else '' }}</textarea><br>
+        <textarea name="sql_query" rows="21" cols="150">{{ sql_query if sql_query else '' }}</textarea><br>
         <input type="submit" value="Executar">
     </form>
     {% if result %}
@@ -46,6 +46,8 @@ HTML_TEMPLATE = """
 @app.route('/', methods=['GET', 'POST'])
 def query_interface():
     result = None
+    sql_query = ""
+
     if request.method == 'POST':
         sql_query = request.form['sql_query']
         try:
@@ -58,18 +60,57 @@ def query_interface():
         except Exception as e:
             result = [("Erro:", str(e))]
 
-    return render_template_string(HTML_TEMPLATE, result=result)
+    return render_template_string(HTML_TEMPLATE, result=result, sql_query=sql_query)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5001) 
     
     
     
     
-# exemplos de consultas 
+# exemplos de consultas Mysql/Mariadb
 
 '' '''
 SELECT NomeMunicipioSIAFI, COUNT(DISTINCT CPFBeneficiario) AS NumeroBeneficiarios
 FROM Saques
 GROUP BY NomeMunicipioSIAFI;
+
+# informações sobre o banco 
+
+SELECT 
+    TABLES.TABLE_NAME AS 'Table',
+    COLUMNS.COLUMN_NAME AS 'Column',
+    COLUMNS.COLUMN_TYPE AS 'Data Type',
+    COLUMNS.IS_NULLABLE AS 'Is Nullable',
+    CASE
+        WHEN KEY_COLUMN_USAGE.CONSTRAINT_NAME = 'PRIMARY' THEN 'YES'
+        ELSE 'NO'
+    END AS 'Primary Key'
+FROM 
+    INFORMATION_SCHEMA.TABLES
+JOIN 
+    INFORMATION_SCHEMA.COLUMNS ON TABLES.TABLE_NAME = COLUMNS.TABLE_NAME
+LEFT JOIN 
+    INFORMATION_SCHEMA.KEY_COLUMN_USAGE ON TABLES.TABLE_NAME = KEY_COLUMN_USAGE.TABLE_NAME
+    AND COLUMNS.COLUMN_NAME = KEY_COLUMN_USAGE.COLUMN_NAME
+WHERE 
+    TABLES.TABLE_SCHEMA = 'BolsaFamilia' AND COLUMNS.TABLE_SCHEMA = 'BolsaFamilia'
+ORDER BY 
+    TABLES.TABLE_NAME, COLUMNS.ORDINAL_POSITION;
+
+
+
+#Total de Pagamentos por Estado:
+
+SELECT UF, SUM(ValorParcela) AS TotalSaques
+FROM Saques
+GROUP BY UF
+ORDER BY SUM(ValorParcela) DESC
+LIMIT 100000;
+
+
+
+
+
 ''' ''
- 
+
